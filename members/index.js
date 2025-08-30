a40929ba058a8cf5c955067a8e6f43fc380d3bd6
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-30 08:00:10 +0300

    feat: initial commit for members/index.js

---
+++ members/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+
+const mongoURI = 'mongodb://mongo:27017/library';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB kết nối thành công'))
+  .catch((err) => console.log(err));
+
+app.listen(3000, () => {
+  console.log('Quản lý thành viên đang chạy trên cổng 3000');
+});
